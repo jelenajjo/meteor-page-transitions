0c868309de32742fadd137aee34cc1ace56dd76e
Jelena Jovanovic <jelena.jjo@gmail.com>
2014-09-20 17:08:34 +0200

    meteor-page-transitions

---
+++ package.js
@@ -0,0 +1,14 @@
+Package.describe({
+  summary: "A simple responsive layout with some fancy page transitions.",
+  version: "1.0.0",
+  name: "jelena:meteor-page-transitions",
+  git: "https://github.com/jelenajjo/meteor-page-transitions.git"
+});
+
+
+Package.onUse(function (api) {
+  api.addFiles('component.css', 'client');
+  api.addFiles('modernizr.custom.js', 'client');
+  api.addFiles('boxlayout.js', 'client');
+});
+
